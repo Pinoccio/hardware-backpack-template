cce92ef7c3a8357351c9197c4bbd04aeef7aba37
Matthijs Kooijman <matthijs@stdin.nl>
2014-03-11 10:02:15 +0100

    firmware-test: Add crc implementation
    These files were already used, but never got added to the repository (so
    the sketch wouldn't compile before).

---
+++ firmware/test/crc.h
@@ -0,0 +1,29 @@
+// 8-bit CRC implementation
+//
+// Copyright (c) 2013, Matthijs Kooijman <matthijs@stdin.nl>
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+// THE SOFTWARE.
+
+#ifndef _CRC_H
+#define _CRC_H
+
+#include <stdint.h>
+
+uint8_t crc_update(const uint8_t poly, uint8_t crc, uint8_t data);
+#endif // CRC_H
